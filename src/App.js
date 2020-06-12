7db493f24caa90d71c40c684e300bd901e7abf96
waseemrizi <waseemrizi@outlook.com>
2020-06-13 01:24:21 +0500

    inittial commit
    in which i used useState hooks and changed color of background

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {Message} from './Message.js'
 
-function App() {
+export default function App() {
+
+let [count , setCount] = useState(1);
+let [isMorning, setMorning] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className={`box ${isMorning ? 'dayLight' : ''}`}>
+      <h1>Good {isMorning ? 'Morning' : 'Night'}</h1>
+
+      <Message counter={count}/ >
+
+      <br/>
+      
+
+      <button onClick={ 
+        ()=> setCount(count +1)
+         }>
+           Button Update
+           </button>
+
+          
+
+          <button onClick={
+            ()=> setMorning(!isMorning)
+          }>
+            Update day
+          </button>
+
+
+
     </div>
+
   );
 }
 
-export default App;
+
---
+++ src/Message.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export function Message(props){
+    
+    return(
+        <h3>valu of counter variable is: {props.counter}</h3>
+    );
+}
