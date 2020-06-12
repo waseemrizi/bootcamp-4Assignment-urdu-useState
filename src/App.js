2c3bb1f1c7b7361554b34c812ca408f5693d86c9
waseemrizi <waseemrizi@outlook.com>
2020-06-13 01:26:00 +0500

    little changed

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ let [isMorning, setMorning] = useState(false);
   return (
 
     <div className={`box ${isMorning ? 'dayLight' : ''}`}>
-      <h1>Good {isMorning ? 'Morning' : 'Night'}</h1>
+      <h1> Good {isMorning ? 'Morning' : 'Night'}</h1>
 
       <Message counter={count}/ >
 
